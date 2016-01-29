c39d9f6de5deea7dcb951c75d0b21c138d9e6603
Kayne Khoury <kaynekhoury1@gmail.com>
2016-01-28 18:01:50 -0600

    App structure built

--- src/cannata/Customer/CustomerApp.java
+++ src/cannata/Customer/CustomerApp.java
@@ -1,10 +1,10 @@
-package cannata;
+package cannata.Customer;
 
-public class HelloWorld {
+public class CustomerApp {
 
     public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-         System.out.println("Hello, World");
+         System.out.println("What, Up");
     }
 
 }
